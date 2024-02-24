c484769c6255f4a4ea271683f4cc3f927b71ae24
Korny94 <karlmagnusnokling@hotmail.no>
2024-02-24 13:28:59 +0100

    Style

--- src/Components/Layouts/Nav.jsx
+++ src/Components/Layouts/Nav.jsx
@@ -2,16 +2,41 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Search from "./Search/Search.jsx";
 import logo from "../../assets/logo.png";
+import shoppingCart from "../../assets/shoppingCart.png";
+import contact from "../../assets/contact.png";
 
 const StyledNavItems = styled.div`
   list-style: none;
-  font-size: 1.2rem;
-  width: 120px;
-  height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-bottom: 0.2rem;
+
+  padding: 0.5rem 1.5rem;
+
+  font-size: 1.2rem;
+  border: none;
+  outline: none;
+  border-radius: 30px;
+  cursor: pointer;
+  background-color: rgb(14, 14, 26);
+  transition: 0.6s;
+  box-shadow: 0px 0px 60px #1f4c65;
+  -webkit-box-reflect: below 10px
+    linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2));
+
+  &:active {
+    scale: 0.92;
+  }
+
+  &:hover {
+    background: rgb(2, 29, 78);
+    background: linear-gradient(
+      270deg,
+      rgba(2, 29, 78, 0.681) 0%,
+      rgba(31, 215, 232, 0.873) 200%
+    );
+    color: rgb(4, 4, 38);
+  }
 `;
 
 const StyledNav = styled.nav`
@@ -25,15 +50,28 @@ function Nav() {
     <StyledNav>
       <StyledNavItems>
         <Link to="/">
-          <img src={logo} className="logo" alt="logo" />
+          <img src={logo} title="Home" className="logo" alt="logo" />
         </Link>
-        <Link to="/">Name</Link>
       </StyledNavItems>
       <StyledNavItems>
-        <Link to="/contact">Contact Icon</Link>
+        <Link to="/contact">
+          <img
+            src={contact}
+            className="navIcon"
+            title="Contact"
+            alt="contact"
+          />
+        </Link>
       </StyledNavItems>
       <StyledNavItems>
-        <Link to="/cart">Cart Icon</Link>
+        <Link to="/cart">
+          <img
+            src={shoppingCart}
+            className="navIcon"
+            title="Shopping Cart"
+            alt="shoppingCart"
+          />
+        </Link>
       </StyledNavItems>
       <Search />
     </StyledNav>
