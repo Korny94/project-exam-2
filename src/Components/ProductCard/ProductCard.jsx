a304103ffc2bd9f7f98f75ea2b8de6531bac1016
Korny94 <karlmagnusnokling@hotmail.no>
2024-02-27 00:15:05 +0100

    Add props

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -71,7 +71,7 @@ function ProductCard({ product }) {
   return (
     <div className="card">
       <div className="card2">
-        <Link to="/product">
+        <Link to="/product" productInfo={product}>
           <StyledLink>
             {onSale && <StyledSale>Sale!</StyledSale>}{" "}
             {/* Display "Sale" if product is on sale */}
