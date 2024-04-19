058d9cb052488855323cb5d81b63e6041a3f092b
Karl Magnus Leonardo Nøkling <114525120+Korny94@users.noreply.github.com>
2024-04-19 19:30:56 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
 import Product from "./pages/Product/Product.jsx";
 import Cart from "./pages/ShoppingCart/ShoppingCart.jsx";
 import RouteNotFound from "./pages/RouteNotFound/RouteNotFound.jsx";
 import Layout from "./Components/Layouts/Layout.jsx";
-import { useEffect } from "react";
 import CheckoutSuccess from "./pages/CheckoutSuccess/CheckoutSuccess.jsx";
 
 function App() {
-  useEffect(() => {
-    // Update cart count when component mounts
-    const cartCount = document.getElementById("cartCount");
-    const cart = JSON.parse(localStorage.getItem("cart"));
-    if (cart) {
-      cartCount.innerHTML = cart.length;
-    }
-  }, []);
   return (
     <div className="App">
       <Routes>
+        <Route index element={<Login />} />
+
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
           <Route path="product" element={<Product />} />
           <Route path="cart" element={<Cart />} />
           <Route path="contact" element={<Contact />} />
